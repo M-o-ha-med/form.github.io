c1f51a75589925372445e006c1fd09145fe205ee
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2024-11-07 16:24:29 +0700

    Update index_link.php

--- index_link.php
+++ index_link.php
@@ -35,8 +35,36 @@
       
       <?php
       if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $nim = $_GET['NIM'];
+        $nama = $_GET['Nama'];
+        $gender = $_GET['Jenis_kelamin'];
+        $alamat = $_GET['Alamat'];
+        $notelp = $_GET['Nomor_telepon'];
+        $email = $_GET['Email'];
+        
+        // Define data array with a header and input values as separate elements
+        $data = [
+            ["NIM", "Nama", "JenisKelamin", "Alamat", "No.telp", "Email"],
+            [$nim, $nama, $gender, $alamat, $notelp, $email]
+        ];
+        
+        // Set headers to indicate file download
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment; filename="data.csv"');
+        
+        // Open output stream for CSV data
+        $fp = fopen('php://output', 'w');
+        
+        // Write each row to the CSV
+        foreach ($data as $line) {
+            fputcsv($fp, $line);
+        }
+        
+        // Close the file pointer
+        fclose($fp);
+
       echo "<h3>Data berhasil disimpan!</h3>";
-      echo "<a href='download.php'>Download csv</a>";
+     
 
       }
       ?>
