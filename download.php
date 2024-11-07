e06f78e1f24fea87f3e9e026fce294675a5e2542
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2024-11-07 16:21:53 +0700

    Update download.php

--- download.php
+++ download.php
@@ -1,20 +1,33 @@
 <?php
 session_start();
+
+// Get form input values
 $nim = $_GET['NIM'];
 $nama = $_GET['Nama'];
 $gender = $_GET['Jenis_kelamin'];
 $alamat = $_GET['Alamat'];
 $notelp = $_GET['Nomor_telepon'];
 $email = $_GET['Email'];
-$data = array(["NIM","Nama","JenisKelamin","Alamat","No.telp","Email"],["$nim,$nama,$gender,$alamat,$notelp,$email"]);
+
+// Define data array with a header and input values as separate elements
+$data = [
+    ["NIM", "Nama", "JenisKelamin", "Alamat", "No.telp", "Email"],
+    [$nim, $nama, $gender, $alamat, $notelp, $email]
+];
+
+// Set headers to indicate file download
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="data.csv"');
 
+// Open output stream for CSV data
 $fp = fopen('php://output', 'w');
-foreach ( $data as $line ) {
 
+// Write each row to the CSV
+foreach ($data as $line) {
     fputcsv($fp, $line);
 }
-fclose($fp);
 
+// Close the file pointer
+fclose($fp);
+exit;
 ?>
