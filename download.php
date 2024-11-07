b411e2018b632b548fab59c4bc90ee5393ebb32c
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2024-11-07 16:10:16 +0700

    Update download.php

--- download.php
+++ download.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-$nim = $_SESSION['NIM'];
-$nama = $_SESSION['Nama'];
-$gender = $_SESSION['Jenis_kelamin'];
-$alamat = $_SESSION['Alamat'];
-$notelp = $_SESSION['Nomor_telepon'];
-$email = $_SESSION['Email'];
+$nim = $_GET['NIM'];
+$nama = $_GET['Nama'];
+$gender = $_GET['Jenis_kelamin'];
+$alamat = $_GET['Alamat'];
+$notelp = $_GET['Nomor_telepon'];
+$email = $_GET['Email'];
 $data = array(["NIM","Nama","JenisKelamin","Alamat","No.telp","Email"],["$nim,$nama,$gender,$alamat,$notelp,$email"]);
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="data.csv"');
